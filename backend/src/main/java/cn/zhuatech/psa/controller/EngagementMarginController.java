7151c3a6ff076bb1bd2df0d76d96916996f8e1b4
nero <233401861@qq.com>
2026-08-22 10:31:26 +0800

    chore: add website to copyright headers

--- backend/src/main/java/cn/zhuatech/psa/controller/EngagementMarginController.java
+++ backend/src/main/java/cn/zhuatech/psa/controller/EngagementMarginController.java
@@ -1,4 +1,4 @@
-/* Copyright 2026 上海如静知华信息科技有限公司 */
+/* Copyright 2026 上海如静知华信息科技有限公司 · https://www.zhuatech.cn/ */
 package cn.zhuatech.psa.controller;
 import cn.zhuatech.psa.common.ApiResponse; import cn.zhuatech.psa.service.EngagementMarginService; import jakarta.validation.Valid; import org.springframework.web.bind.annotation.*;
 @RestController @RequestMapping("/api/admin/engagement-margin") public class EngagementMarginController {
